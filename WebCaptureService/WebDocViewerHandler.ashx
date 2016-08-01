a6a23dd09422d34f8e308ebace9f858b5c4fcb93
Denis Legashov <denis.legashov@gmail.com>
2016-08-01 19:57:20 +0400

    Added WebCaptureService tutorial

---
+++ WebCaptureService/WebDocViewerHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="WebDocViewerHandler.ashx.cs" Class="WebCaptureService.WebDocViewerHandler" %>
--- WebDocumentViewer/Properties/AssemblyInfo.cs
+++ WebDocumentViewer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nuget_tutorials")]
+[assembly: AssemblyTitle("WebDocumentViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nuget_tutorials")]
+[assembly: AssemblyProduct("WebDocumentViewer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
